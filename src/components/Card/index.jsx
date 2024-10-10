3d2e48540b8ca5666562deb5457d9a0d9f269643
Caio <caioosalencar@email.com>
2024-10-09 22:17:06 -0400

    terceiro commit

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,20 +1,23 @@
-import { Clock, Soup } from "lucide-react";
+import { Clock, Soup, Timer } from "lucide-react";
 import Image from "next/image";
 
-export default function Card() {
+export default function Card({imagem, titulo, tempo, serve}) {
     return(
         <div className="m-4 rounded border-2 border-solid border-rose-400">
-            <Image className="bg-cover bg-center" src="/" width={387} height={297} alt="Prato de ovo com pepino"/>
+            <Image 
+            src={`/${imagem}`} 
+            width={387} 
+            height={297} 
+            alt={titulo}/>
             <h2 className="p-2 bg-rose-300 text-gray-800">
-                Ovo com arroz e pepino
+                {titulo}
             </h2>
-            <div className="p-2 text-xs flex items-center gap-2">
-                <Clock />
-                25 minutos
+            <div className="p-2 text-xs flex items-center gap-1">
+                <Timer/>    {tempo} minuto(s)
             </div>
-            <div className="p-2 text-xs flex items-center gap-2">
+            <div className="p-2 text-xs flex items-center gap-1">
                 <Soup />
-                serve 2 pessoas
+                serve {serve} pessoa(s)
             </div>
         </div>
     ) 
--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -6,21 +6,28 @@ import Card from "../Card";
 export default function CardList(){
     
     const [cards, setCards] = useState([
-        {id: 1, titulo: "ovo com Arroz e pepipo", tempo: 25, server: 2}
-        {id: 2, titulo: "ovo mexido", tempo: 20, server: 2}
-        {id: 3, titulo: "ovo com macarrão", tempo: 25, server: 1}
-        {id: 4, titulo: "Pão com ovo", tempo: 25, server: 1}
+        {id: 1, titulo: "ovo com Arroz e pepipo",   imagem: "Ovo-pepino.png", tempo: 25, server: 2},
+        {id: 2, titulo: "ovo mexido",               imagem: "Ovo-pepino.png", tempo: 20, server: 2},
+        {id: 3, titulo: "ovo com macarrão",         imagem: "Ovo-pepino.png", tempo: 25, server: 1},
+        {id: 4, titulo: "Pão com ovo",              imagem: "Ovo-pepino.png", tempo: 25, server: 1}
     ])
     
     return(
         <div className="flex flex-wrap flex-col m-8">
             <div>Quantidade de receitas: {cards.length}</div>
-            <ul className="list-disc">
-                {cards.map(c => (
-                    <li key={c.id}>{c.titulo} - serve {c.server} pessoa(s) </li>
-                ))}
+            
+            <div className="flex flex-wrap">
+                    {cards.map(c => (
+                        <Card 
+                        key={c.id}
+                        imagem={c.imagem}
+                        titulo={c.titulo}
+                        tempo={c.tempo}
+                        serve={c.serve}
+                        />
+                    ))}
+            </div>
 
-            </ul>
         </div>
     )
 }
